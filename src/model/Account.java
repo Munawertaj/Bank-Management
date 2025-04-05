872a263422967b6868085032f4d0b21f7fed3cd1
Munawer-Taj <taj4580@gmail.com>
2025-04-05 19:51:02 +0600

    Transaction list added into Account class

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,14 +1,19 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
     private int accountNumber;
     private double balance;
     private Customer customer;
+    private List<Transaction> transactions;
 
     public Account(int accountNumber, double balance, Customer customer) {
         this.accountNumber = accountNumber;
         this.balance = balance;
         this.customer = customer;
+        this.transactions = new ArrayList<Transaction>();
     }
 
     public int getAccountNumber() {
@@ -21,6 +26,10 @@ public class Account {
 
     public Customer getCustomer() {
         return customer;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
     }
 
     public void setBalance(double balance) {
