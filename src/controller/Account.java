aab0444a7766ef6fd31b2c1b23780b0ff6a4f167
Munawer-Taj <taj4580@gmail.com>
2025-04-05 18:43:11 +0600

    Account and Customer class created

---
+++ src/controller/Account.java
@@ -0,0 +1,29 @@
+package controller;
+
+public class Account {
+    private int accountNumber;
+    private double balance;
+    private Customer customer;
+
+    public Account(int accountNumber, double balance, Customer customer) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.customer = customer;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
